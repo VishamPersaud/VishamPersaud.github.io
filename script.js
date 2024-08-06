d1cb4331595b50e50adf2b909ba86d36f0d47f65
VishamPersaud <vishamp0@gmail.com>
2024-08-06 18:22:31 +0000

    A Test DOM Event listener

---
+++ script.js
@@ -0,0 +1,10 @@
+let mybutton = document.querySelector("button");
+
+function happyClick() {
+    document.querySelector("#deadpool").src = "https://heroichollywood.com/wp-content/uploads/2020/02/Ryan_Reynolds_David_Leitch_Deadpool.jpg"
+
+mybutton.addEventListener('click', happyClick);
+
+ 
+     
+ 
